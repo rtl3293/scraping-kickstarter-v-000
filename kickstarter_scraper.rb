d1a0b121d42b4a606bd5da108f8f6cd8b46b3168
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-03 17:33:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ def create_project_hash
    #titles: project.css("h2.bbcard_name strong a").text
    #image link: project.css("div.project-thumbnail a img").attribute("src").value
    #description: project.css("p.bbcard_blurb").text
+   #location: project.css("ul.project-meta li a span.location-name").text
 end
 
 create_project_hash
